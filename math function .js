171b68138b6cb7834249de605afd03cbaba88606
Ritik <ritikkhandelwal650@gmail.com>
2022-09-30 03:50:28 -0700

    math function

---
+++ math function .js
@@ -0,0 +1,27 @@
+// square root 
+
+const a = Math.sqrt(4);
+console.log(a); 
+
+// power 
+const c = Math.pow(5,2);
+console.log(c);
+
+//round 
+const d = Math.round(8.9);
+console.log(d);
+
+const e = Math.round(-8.5);
+console.log(e);
+
+//ceil
+const f = Math.ceil(-9.7);
+console.log(f);
+
+//floor
+const g = Math.floor(-9.5);
+console.log(g);
+
+//cuberoot
+const h = Math.cbrt(8);
+console.log(h);
